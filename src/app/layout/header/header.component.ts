6f17588c55bc11549b97699c22f031620b3422e0
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-27 19:19:59 +0530

    🚩 firebase auth , footer & header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  email = null;
+  constructor(
+    private auth: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    auth.getUser().subscribe((user) => {
+      console.log(user);
 
-  constructor() { }
-
-  ngOnInit(): void {
+      this.email = user?.email;
+    });
   }
 
+  ngOnInit(): void {}
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+      this.router.navigateByUrl('signin');
+      this.toastr.info('Logout Successful');
+      this.email = null;
+    } catch (error) {
+      this.toastr.error('Something wrong! SignOut unsuccessful');
+    }
+  }
 }
